cff3525d3ef82f41dc6211e301c592e2aed63668
Bohdan <bohdan.solomakha@gmail.com>
2022-05-06 20:44:21 +0300

    changed base url

--- src/API/pizzasAPI.js
+++ src/API/pizzasAPI.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const pizzasAPI = {
   getPizzas(sortBy, categories) {
     return axios.get(
-      `http://localhost:3001/pizzas?${
+      `/pizzas?${
         categories > 0 ? `category=${categories}` : ""
       }&_sort=${sortBy}&_order=desc`
     );
