69dbdb94545f3c8bee342d1bfa3d72e298168209
Bohdan <bohdan.solomakha@gmail.com>
2022-05-06 19:57:57 +0300

    Made some changes for deploying on Heroku

--- src/API/pizzasAPI.js
+++ src/API/pizzasAPI.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+export const BASE_URL = "https://react-pizza-solo.herokuapp.com/api"
 export const pizzasAPI = {
   getPizzas(sortBy, categories) {
     return axios.get(
-      `/pizzas?${
+      `${BASE_URL}/pizzas?${
         categories > 0 ? `category=${categories}` : ""
       }&_sort=${sortBy}&_order=desc`
     );
